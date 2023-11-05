f87c12260f958765b2a70095be4542b2a1815e6a
sofiahag <sofia.h4glund@gmail.com>
2023-11-05 14:53:55 +0100

    try again

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+            fs: false,
+            };
+        }
+        return config;
+    },
+};
 
 module.exports = nextConfig
