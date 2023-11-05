e2bb3aa72dab5f1d89d1f8ca31dd6960c1401aee
sofiahag <sofia.h4glund@gmail.com>
2023-11-05 14:34:02 +0100

    fine, remove fb config from gitignore

---
+++ src/app/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdWuWZHhnCKiSQ1Sk5EDgP1MjE-YUCrV8",
+    authDomain: "sofiahag-todo-db.firebaseapp.com",
+    projectId: "sofiahag-todo-db",
+    storageBucket: "sofiahag-todo-db.appspot.com",
+    messagingSenderId: "219163789306",
+    appId: "1:219163789306:web:f3c854a0b6a4b7738a4643",
+    measurementId: "G-RZ6Z3VXKDD"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+export { db }
