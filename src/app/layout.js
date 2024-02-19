d923c1372942e2b54fd47601d6d72b38ee1e0fff
sofiahag <sofia.h4glund@gmail.com>
2024-02-19 21:19:15 +0100

    change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,6 @@ import './globals.css'
 
 import { Kalam } from 'next/font/google'
 
-const kalam = Kalam({
-  subsets: ['latin'],
-  variable: '--font-kalam',
-  weight: '400'
-})
 
 export const metadata = {
   title: 'Todo React App',
@@ -19,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={kalam.className}>{children}</body>
+      <body className="text-sofia">{children}</body>
     </html>
   )
 }
